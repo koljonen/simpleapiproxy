86ca71191299bbe4b89e18c612993e3d9207855a
Joakim Koljonen <joakim.koljonen@trustly.com>
2019-11-24 14:15:24 +0100

    Try to fix duplicate-header problem

--- apiproxy/apiproxy.go
+++ apiproxy/apiproxy.go
@@ -44,6 +44,7 @@ func queryCombiner(handler http.Handler, addon string) http.Handler {
 // Allow cross origin resource sharing
 func addCORS(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Del("Access-Control-Allow-Origin")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With")
 		handler.ServeHTTP(w, r)
